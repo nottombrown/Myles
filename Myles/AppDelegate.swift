91e435c6c84e31bad19b07f2670336e09816589d
Tom Brown <nottombrown@gmail.com>
2016-01-20 09:38:02 -0800

    Install Parse

--- Myles/AppDelegate.swift
+++ Myles/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios/guide#local-datastore
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("p0FWSMkS29oKo0YuBYvQeIFTpWtvRWCwaIHTHjYr",
+            clientKey: "zhFXvJlqtEBPz4Mml76OlFkiTUdMugdhAGA3qnKf")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         // Override point for customization after application launch.
         return true
     }
