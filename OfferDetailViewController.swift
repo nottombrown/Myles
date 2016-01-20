8e242989a16168d3b63b0069d4dad37577734742
Tom Brown <nottombrown@gmail.com>
2016-01-20 12:25:23 -0800

    Create OfferDetailView and Controller

---
+++ OfferDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OfferDetailViewController.swift
+//  Myles
+//
+//  Created by Tom Brown on 1/20/16.
+//  Copyright © 2016 tombarr. All rights reserved.
+//
+
+import Foundation
+
+class OfferDetailViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
